35cfacbd00da29d20f90e2575da47f94e2e7e709
AlexLiZL <lixwhy@gmail.com>
2018-09-05 15:44:44 +0800

    Update File Header.java

--- .idea/fileTemplates/includes/File Header.java
+++ .idea/fileTemplates/includes/File Header.java
@@ -1,14 +1,7 @@
 /* *****************************************************************************
- *  Name:    Alan Turing
- *  NetID:   aturing
- *  Precept: P00
- *
- *  Partner Name:    Ada Lovelace
- *  Partner NetID:   alovelace
- *  Partner Precept: P00
+ *  Name:    Alex Li
+ *  NetID:   AlexLiZL
  * 
- *  Description:  Prints 'Hello, World' to the terminal window.
- *                By tradition, this is everyone's first program.
- *                Prof. Brian Kernighan initiated this tradition in 1974.
+ *  Description:  Doing Excersice for ALGS4, Chapter 1.
  *
  **************************************************************************** */
